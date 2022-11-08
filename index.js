98ecc997472e8e2efd589910e44637c3c3fc78ac
mcuzbgamer <111974683+mcuzbgamer@users.noreply.github.com>
2022-11-08 15:39:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host:'ir.skyblock.uz',
-  // port: 57285,
-  username: 'KomiljonHelper'
- })
- 
- let continue_digging = true; 
- 
-bot.once("spawn", () => {
-  bot.chat("/login fazliddinov ");
+ host:'ir.skyblock.uz',
+ port: 25566,
+ username: 'KomiljonHelper'
 })
-
-
-bot.on('chat', (username, message) => {
-  if (message == 'kovla_helper') {
-    continue_digging=true;
-    dig()  
+bot.on('messagestr', (message) => {
+  if(message.includes("/login fazliddinov"))
+  {
+    bot.chat("/login fazliddinov")
   }
-})
+  
 
+}
+
+
+)
+bot.on('messagestr', (message) => {
+  if(message.includes("/log"))
+  {
+    bot.chat("/login jobir2009")
+  }
+  
+
+}
+
+
+)
 async function dig() {
-  if(!continue_digging){return};
   if (!bot.heldItem || !bot.heldItem.name.includes('pickaxe')) {
-    var pickaxe = bot.inventory.items().filter(i => i.name.includes('pickaxe'))[0]; 
-  if (pickaxe) await bot.equip(pickaxe, 'hand')
+    var pickaxe = bot.inventory.items().filter(i => i.name.includes('pickaxe'))[0];
+    if (pickaxe) await bot.equip(pickaxe, 'hand')
+    if(!pickaxe) bot.quit();
   }
-  var block = bot.blockAtCursor(4);
-  if (!block) return setTimeout (function () { dig(); }, 100);
-  await bot.dig(block, 'ignore')
+  var block = bot.blockAtCursor(7);
+  if (!block) return setTimeout(function() {
+    dig();
+  }, 100);
+  await bot.dig(block, 'ignore', 'raycast')
   dig()
 }
+bot.once("spawn", () => {
+    setTimeout(() => {
+        dig();
+   }, 20000); 
+})
+bot.on('chat', (username, message) => {
+    if (username === 'FamousTrain1733') {
+    if (message.indexOf('@') !== -1) {
+            var replacement = "@",
+                toReplace = "",
+                str = message
+
+            str = str.replace(replacement, toReplace)
+            bot.chat(str)
+        }}})
+    bot.on('kicked', console.log)
+bot.on('error', console.log)
