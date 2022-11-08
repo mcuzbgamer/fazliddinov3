6f4d5e9e4a9db8c646b8926e6f1704a53ad817ce
mcuzbgamer <111974683+mcuzbgamer@users.noreply.github.com>
2022-11-08 16:00:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mineflayer = require('mineflayer')
 const bot = mineflayer.createBot({
  host:'ir.skyblock.uz',
  port: 25566,
- username: 'Jabikminer'
+ username: 'KomiljonHelper'
 })
 bot.on('messagestr', (message) => {
   if(message.includes("/register"))
